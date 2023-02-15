ec3df801d73a76cd8089ee817a77067861227193
Farah Mdh <mdhfarah3@gmail.com>
2023-02-15 13:50:15 +0100

    Amélioration du HTML et CSS

--- header.php
+++ header.php
@@ -26,8 +26,6 @@
         <!--NAVIGATION BAR-->
 
 <nav id="menu-item" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement" class="PageHeader__nav">
-            <!-- <a class="PageHeader__nav-contact" href="nous-rencontrer">Nous rencontrer</a> 
-            <a class="PageHeader__nav-admin" href="admin">Admin</a> -->
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>', 'container' => '' ) ); ?>
 <a class="PageHeader__nav-command" href="commander">Commander</a>
 </nav>
